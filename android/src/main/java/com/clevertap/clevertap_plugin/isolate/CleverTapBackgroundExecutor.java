777a85c4dfb3eb9cef6caf843a2bed6f357f1c16
Shivam Sharma <shivam.sharma@clevertap.com>
2023-07-11 14:29:57 +0530

    some cleanup

--- android/src/main/java/com/clevertap/clevertap_plugin/isolate/CleverTapBackgroundExecutor.java
+++ android/src/main/java/com/clevertap/clevertap_plugin/isolate/CleverTapBackgroundExecutor.java
@@ -1,7 +1,3 @@
-// Copyright 2020 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 package com.clevertap.clevertap_plugin.isolate;
 
 import android.content.Context;
