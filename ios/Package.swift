b3d30e81acef1f2ad50ba6947ac7cdcd0ccba393
ReshabCT <reshab@clevertap.com>
2026-05-08 00:23:28 +0530

    Create Package.swift file for spm support

---
+++ ios/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "clevertap_plugin",
+    platforms: [
+        .iOS("12.0")
+    ],
+    products: [
+        .library(name: "clevertap-plugin", targets: ["clevertap_plugin"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/CleverTap/clevertap-ios-sdk",
+            from: "7.6.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "clevertap_plugin",
+            dependencies: [
+                .product(name: "CleverTapSDK", package: "clevertap-ios-sdk")
+            ],
+            path: "Classes",
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("include"),
+                .headerSearchPath(".")
+            ]
+        )
+    ]
+)
