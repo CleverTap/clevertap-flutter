5dd7d9fd76bfae10ce78d5752ab24d3da5a13677
ReshabCT <reshab@clevertap.com>
2026-08-27 10:35:39 +0530

    remove flutterframework dependency from package.swift to support lower flutter sdk versions

--- ios/clevertap_plugin/Package.swift
+++ ios/clevertap_plugin/Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         .library(name: "clevertap-plugin", targets: ["clevertap_plugin"])
     ],
     dependencies: [
-        .package(name: "FlutterFramework", path: "../FlutterFramework"),
         .package(
             url: "https://github.com/CleverTap/clevertap-ios-sdk",
             exact: "7.8.1"
@@ -20,7 +19,6 @@ let package = Package(
         .target(
             name: "clevertap_plugin",
             dependencies: [
-                .product(name: "FlutterFramework", package: "FlutterFramework"),
                 .product(name: "CleverTapSDK", package: "clevertap-ios-sdk")
             ],
             cSettings: [
